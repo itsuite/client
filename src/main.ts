1855c5fee2df385a0a41c542e7186de9c169bb6e
Michal Marek <michal@marek.pw>
2017-03-05 00:12:22 +0100

    aurelia-bootstrap plugin

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,10 @@ export async function configure(aurelia: Aurelia) {
     aurelia.use
         .standardConfiguration()
         .developmentLogging()
-        .plugin('aurelia-computed', {
+        .plugin('aurelia-computed', { // optimized computing and debug info about dirty checks
             enableLogging: true
-        });
+        })
+        .plugin('aurelia-bootstrap', config => config.options.version = 4); //bootstrap 4 native components
 
     await aurelia.start();
     aurelia.setRoot('app');
