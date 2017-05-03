8b7565a87de40cf1ae76689db92c604bf1883cde
Michal Marek <michal@marek.pw>
2017-05-03 21:17:24 +0200

    WIP basic boards

--- src/apps/home/home-config.ts
+++
@@ -1,9 +0,0 @@
-import {AppConfig} from "src/shared/app-config";
-
-export const HomeConfig: AppConfig = {
-    name: "Homepage",
-    perex: "List apps",
-    route: {
-        name: '/'
-    }
-};
